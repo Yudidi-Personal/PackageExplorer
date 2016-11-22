367e56c3cbb7969ae066c488423f638d88fd92f0
sicnuthinkgeo <mikeyu@thinkgeo.com>
2016-11-22 17:23:55 +0800

    1. 把PacakgeInfoViewModel分为两个类 2. 为了实现第五点需求 "5. Please show all lib, build and dependency, When mouse is placed on a platform." 在PlatformInfo.cs用到3个集合字段.

---
+++ PackageExplorer/PackageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace PackageExplorer
+{
+    public class PackageInfo
+    {
+        private string packageName;
+        private List<PlatformInfo> platformList;
+
+        public string PackageName
+        {
+            get { return packageName; }
+        }
+
+    }
+}
--- PackageExplorer/PlatformInfo.cs
+++ PackageExplorer/PlatformInfo.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace PackageExplorer
 {
-    
-    public class Platform
+    public class PlatformInfo
     {
+        private List<string> libCountMap;
+        private List<string> buildCountMap;
+        private List<string> dependencyCount;
 
+        public List<string> LibCountMap
+        {
+            get { return libCountMap; }
+        }
+
+        public List<string> BuildCountMap
+        {
+            get { return buildCountMap; }
+        }
+
+        public List<string> DependencyCount
+        {
+            get { return dependencyCount; }
+        }
     }
 
 }
